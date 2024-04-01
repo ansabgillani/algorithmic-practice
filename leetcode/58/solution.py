35c0c050dcfd2ff5273428278aa53a467cdba79d
Ansab Gillani <ansabgillani@Ansabs-MacBook-Pro.local>
2024-04-01 21:24:00 +0000

    feat: add problem of the day for 2024-04-01

---
+++ leetcode/58/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # Remove trailing spaces and split the string into words
+        words = s.rstrip().split()
+        
+        # Return the length of the last word
+        return len(words[-1])
