bdc7409c9b99381b898f1028d73fc3db82a089da
Ansab Gillani <ansabgillani@Ansabs-MacBook-Pro.local>
2024-12-02 19:49:00 +0000

    feat: add problem of the day for 2024-12-02

---
+++ leetcode/1455/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+        for index, word in enumerate(words, start=1):
+            if word.startswith(searchWord):
+                return index
+        return -1
